082b18051ada0b6f440a1a17b1ddc12f34628165
Simonvn1 <simon@vannoppen.com>
2018-04-18 14:49:16 +0200

    shitty site

--- frameworktests/Baseguide/test.php
+++ frameworktests/Baseguide/test.php
@@ -9,6 +9,29 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container sass-box"></div>
+    <header class="col sass-box">
+        <div class="col col-xs-4"><h1>HALLOO</h1></div>
+        <div class="col col-xs-1 col-xs-offset-6 sass-box1 dropdown">
+            <ul class="list-unstyled">
+                <li>Hallo</li>
+                <li>heyo</li>
+                <li>doei</li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        <div class="col col-xl-3 sass-col">
+            <div class="artikel">hier staat veel informatie om te kijken of die het onder elkaar zet of dat hij de div stretched</div>
+            <div class="artikel"> hier staat nog een mooie div deze is er om aan het resultaat te beruieken</div>
+        </div>
+        <div class="col col-xl-4 col-xl-offset-1 sass-col row">
+            <div class="col col-xs-6 sass-col artikel">test teksten zijn belangrijk om te zien of het wel echt werkt hoe je wil.</div>
+            <div class="col col-xs-5 sass-col artikel">en zo als je hier zit werkt het inderdaad wel/niet of de manier hoe je het wilt hebben.</div>
+        </div>
+        <div class="col col-xl-3 col-xl-offset-1 sass-col">
+            <div class="artikel">hier staat veel informatie om te kijken of die het onder elkaar zet of dat hij de div stretched</div>
+            <div class="artikel"> hier staat nog een mooie div deze is er om aan het resultaat te beruieken</div>
+        </div>
+    </main>
 </body>
 </html>
