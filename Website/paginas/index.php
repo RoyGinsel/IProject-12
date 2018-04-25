5c75178aef93f079246f232be8bdc3612f74ee89
RoyGinsel <36151106+RoyGinsel@users.noreply.github.com>
2018-04-25 15:11:31 +0200

    commentaar aan index html

--- Website/paginas/index.php
+++ Website/paginas/index.php
@@ -4,6 +4,7 @@ session_start();
   $cookie_name = "callout";
   $cookie_value = "cookieVoorCallout";
 
+//Cookie zetten voor callout
 if(!isset($_COOKIE[$cookie_name])){
     setcookie($cookie_name ,$cookie_value,time() + (86400 * 30),"/",null,null,null);
 }
@@ -27,7 +28,9 @@ if(!isset($_COOKIE[$cookie_name])){
   ?>
 
 <main class="page">
+
   <?php
+  //call-out bericht
 if(!isset($_COOKIE[$cookie_name])) {
     echo  '
     <div class="call-out" uk-alert>
@@ -36,33 +39,35 @@ if(!isset($_COOKIE[$cookie_name])) {
         <p> Veel koop plezier </p> </div>';
 }
     ?>
+<!-- alert van aantal veilingen sinds laatste bezoek toegevoegd -->
     <div class="uk-alert-primary uk-margin-remove" uk-alert>
     <a class="uk-alert-close" uk-close></a>
     <p class= "uk-text-center">Aantal veiling sinds laatste bezoek toegevoegd: ..</p>
 </div>
-<!-- voorfoto -->
+
+<!-- voorfoto slideshow-->
 <div class="uk-position-relative uk-visible-toggle uk-light" uk-slideshow="animation: fade">
 
     <ul class="uk-slideshow-items uk-slid">
         <li>
             <img src="../media/veiling-hamer.jpg" alt="" uk-cover>
             <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom uk-height-max-medium">
-                <h3 class="uk-margin-remove">Bottom</h3>
-                <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h3 class="uk-margin-remove">Eenmaal Andermaal!</h3>
+                <p class="uk-margin-remove">De duurzaam tweedhands item verkoop website van 2k18.</p>
             </div>
         </li>
         <li>
             <img src="../media/veiling-groep.jpg" alt="" uk-cover>
             <div class="uk-overlay uk-overlay-primary uk-position-left uk-text-center uk-transition-slide-left uk-width-medium">
-                <h3 class="uk-margin-remove">Bottom</h3>
-                <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h3 class="uk-margin-remove">Samen</h3>
+                <p class="uk-margin-remove">nemen we stappen naar een duurzame samenleving</p>
             </div>
         </li>
         <li>
             <img src="../media/hamer-huis.jpg" alt="" uk-cover>
             <div class="uk-overlay uk-overlay-primary uk-position-right uk-text-center uk-transition-slide-right uk-width-medium ">
-                <h3 class="uk-margin-remove">Left</h3>
-                <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h3 class="uk-margin-remove">Fantastische items</h3>
+                <p class="uk-margin-remove">De beste, goedkoopste items om op te bieden!</p>
             </div>
         </li>
     </ul>
@@ -73,7 +78,7 @@ if(!isset($_COOKIE[$cookie_name])) {
 </div>
 
 
-<!-- Lijsten -->
+<!-- Lijsten "grid"-->
   <div class="uk-card uk-card-body tabel uk-margin-remove uk-child-width-1-2@m uk-child-width-1-1 uk-width-1-1 uk-flex-inline@m uk-flex-around@m" uk-grid>
 
 <!-- tabel populaire-items -->
@@ -90,6 +95,8 @@ if(!isset($_COOKIE[$cookie_name])) {
         </thead>
         <tbody>
           <?php
+          //gegevens halen uit de database en in tabel body zetten
+
   //         $sql = "SELECT title, image,omschrijving,prijs,link from tabelnaam ";
   //         $query = $dbh->prepare($sql);
   //         $query->execute($sql);
