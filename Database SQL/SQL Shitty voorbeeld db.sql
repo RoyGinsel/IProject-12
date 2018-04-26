31324b47acfb7e83a80a480d8026601f86300371
Simonvn1 <simon@vannoppen.com>
2018-04-26 15:04:53 +0200

    HALLLOO SQL HIER

---
+++ Database SQL/SQL Shitty voorbeeld db.sql
@@ -0,0 +1,54 @@
+create database EenmaalAndermaal
+
+create table tblRubriek(
+	rubriekNummer int not null,
+	rubriekNaam varchar(100) not null,
+	parentRubriek int null,
+	volgnr int not null,
+	constraint pk_tblRubriek primary key (rubriekNummer),
+	constraint fk_rubriek_rubriek foreign key (parentRubriek) references tblRubriek(rubriekNummer),
+	constraint ck_rubrieknummer_volgnr check (rubriekNummer = volgnr)
+)
+
+create table tblVoorwerp(
+	voorwerpNummer bigint not null,
+	titel varchar(50) not null,
+	beschrijving varchar(max) not null,
+	startPrijs numeric(11,2) not null,
+	betaalWijze varchar(16) not null,
+	betalingsInstructie varchar(max) null,
+	plaatsnaam varchar(60) not null,
+	land varchar (50) not null,
+	looptijd int not null,
+	looptijdBeginDag date not null,
+	looptijdBeginTijdstip time not null,
+	verzendkosten numeric(5,2) null,
+	verzendInstructie varchar(max) null,
+	verkoper varchar(20) not null,
+	koper varchar(20) null,
+	looptijdEindeDag date not null,
+	looptijdEindeTijdstip time not null,
+	veilingGesloten bit not null,
+	verkoopPrijs numeric (11,2) null,
+	constraint pk_tblVoorwerp primary key(voorwerpnummer)
+)
+
+create table tblVoorwerpRubriek(
+	voorwerpNummer bigint not null,
+	rubriekNummer int not null,
+	constraint pk_tblVoorwerpRubriek primary key (voorwerpNummer,rubriekNummer),
+	constraint fk_tblVoorwerpRubriek_voorwerpNummer foreign key (voorwerpNummer) references tblVoorwerp(voorwerpNummer),
+	constraint fk_tblVoorwerpRubriek_rubriekNummer foreign key (rubriekNummer) references tblRubriek(rubriekNummer)
+)
+
+insert into tblRubriek values
+	(1,'Auto',null,1),
+	(2,'Aston Martin',1,2)
+
+
+insert into tblVoorwerp values
+	(1,'Aston Martin Vulcan','Hele mooie waggie jonguh',2000000.20,'PayPal','gimme da money','Loo','Nederland','5','4/26/2018','14:48:00.0000',3.50,'in da box',20,20,'4/30/2018','14:48:00.0000',0,null),
+	(2,'Diploma HBO-ICT','bespaat jezelf 4 jaar',1.50,'PayPal','gimme da money','Loo','Nederland','3','4/26/2018','14:48:00.0000',3.50,'in da box',20,20,'4/29/2018','14:48:00.0000',0,null)
+
+insert into tblVoorwerpRubriek values
+	(1,1)
