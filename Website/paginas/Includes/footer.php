6630b29b3ba13900ca14eb0caa01584b2110214e
Simonvn1 <simon@vannoppen.com>
2018-04-26 10:19:43 +0200

    gotta take them breaks

--- Website/paginas/Includes/footer.php
+++ Website/paginas/Includes/footer.php
@@ -1,4 +1,5 @@
 <footer class="uk-width-1-1">
 <h4><a href="#top" class="back-to-top">Back to top</a></h4>
 Eenmaal Andermaal is niet aansprakelijk voor (gevolg)schade die voortkomt uit het gebruik van deze site, dan wel uit fouten of ontbrekende functionaliteiten op deze site. Copyright © 2018 IConcepts B.V. Alle rechten voorbehouden.
+<br>
 </footer>
