b991a417b3f84807cf7a3b22a3f658ca80949f81
RoyGinsel <36151106+RoyGinsel@users.noreply.github.com>
2018-05-03 23:41:39 +0200

    rubriekenboomheaderdropdown

---
+++ Website/paginas/Includes/Rubriekenboom-header-dropdown.php
@@ -0,0 +1,59 @@
+<!-- No card -->
+<div class="uk-width-1-2 uk-width-1-3@m uk-flex-between" uk-grid>
+  <div>
+    <div class="uk-card uk-card-default uk-card-body">
+      <ul class="uk-nav uk-dropdown-nav uk-list uk-list-large">
+          <li class="uk-active"><a href="#">Active</a>
+            <ul class="uk-subnav">
+              <li>subrubriek</li>
+            </ul>
+          </li>
+          </ul>
+    </div>
+
+  </div>
+  <div class="uk-flex-last">
+    <div class="uk-card uk-card-secondary uk-card-body">
+      <ul class="uk-nav uk-dropdown-nav uk-list uk-list-large">
+          <li class="uk-active"><a href="#">Active</a>
+            <ul class="uk-subnav">
+              <li>subrubriek</li>
+            </ul>
+          </li>
+          </ul>
+    </div>
+  </div>
+  <div>
+    <div class="uk-card uk-card-default uk-card-body">Item 3</div>
+  </div>
+</div>
+
+<div class="uk-width-1-2 uk-width-1-3@m uk-flex-between uk-margin-remove" uk-grid>
+  <div>
+    <div class="uk-card uk-card-default uk-card-body">
+      <ul class="uk-nav uk-dropdown-nav uk-list uk-list-large">
+          <li class="uk-active"><a href="#">Active</a>
+            <ul class="uk-subnav">
+              <li>subrubriek</li>
+            </ul>
+          </li>
+          </ul>
+    </div>
+
+  </div>
+  <div class="uk-flex-last">
+    <div class="uk-card uk-card-secondary uk-card-body">
+      <ul class="uk-nav uk-dropdown-nav uk-list uk-list-large">
+          <li class="uk-active"><a href="#">Active</a>
+            <ul class="uk-subnav">
+              <li>subrubriek</li>
+            </ul>
+          </li>
+          </ul>
+    </div>
+  </div>
+  <div>
+    <div class="uk-card uk-card-default uk-card-body">Item 3</div>
+  </div>
+
+</div>
