131c7719cd72d33cc2e4a30cd8f0c422c8b3b509
Simonvn1 <simon@vannoppen.com>
2018-05-07 16:15:13 +0200

    Revert "Revert "test""
    This reverts commit ca30caa5fadc00ec3f6579522b0ac09948b4c3ba.

--- Website/paginas/Includes/Rubriekenboom-header-dropdown.php
+++ Website/paginas/Includes/Rubriekenboom-header-dropdown.php
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
   <div>
     <button class="uk-button uk-button-default" type="button" uk-toggle="target: #toggle-usage">Toggle</button>
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 
@@ -8,20 +8,20 @@
 >>>>>>> parent of 99eb439... rubriekding
 =======
 >>>>>>> parent of 5963d42... Merge branch 'master' of https://github.com/Simonvn1/IProject-12
+=======
+>>>>>>> parent of e1b2a27... was deze
 <div class="uk-padding-small uk-nav uk-dropdown-nav">
   <h3>postzegels</h3>
-  <ul>
+  <ul class="rubriekenitem">
     <li>europa</li>
     <li>amerika</li>
   </ul>
 <<<<<<< HEAD
 </div>
-</div>
 
-<div>
 <div class="uk-padding-small uk-nav uk-dropdown-nav">
   <h3>postzegels</h3>
-  <ul>
+  <ul class="rubriekenitem">
     <li>europa</li>
     <li>amerika</li>
   </ul>
@@ -39,14 +39,28 @@
 </div>
 >>>>>>> parent of 5963d42... Merge branch 'master' of https://github.com/Simonvn1/IProject-12
 </div>
-</div>
 
-<div>
 <div class="uk-padding-small uk-nav uk-dropdown-nav">
   <h3>postzegels</h3>
-  <ul>
+  <ul class="rubriekenitem">
     <li>europa</li>
     <li>amerika</li>
   </ul>
 </div>
+
+<div class="uk-padding-small uk-nav uk-dropdown-nav">
+  <h3>postzegels</h3>
+  <ul class="rubriekenitem">
+    <li>europa</li>
+    <li>amerika</li>
+  </ul>
+</div>
+
+
+<div class="uk-padding-small uk-nav uk-dropdown-nav">
+  <h3>postzegels</h3>
+  <ul class="rubriekenitem">
+    <li>europa</li>
+    <li>amerika</li>
+  </ul>
 </div>
