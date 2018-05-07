d6adf1421cecdb096829608209c261846dbc4957
Simonvn1 <simon@vannoppen.com>
2018-05-07 16:14:15 +0200

    no

--- Website/paginas/Includes/Rubriekenboom-header-dropdown.php
+++ Website/paginas/Includes/Rubriekenboom-header-dropdown.php
@@ -1,16 +1,20 @@
 <<<<<<< HEAD
   <div>
     <button class="uk-button uk-button-default" type="button" uk-toggle="target: #toggle-usage">Toggle</button>
+<<<<<<< HEAD
 =======
 
 <div>
 >>>>>>> parent of 99eb439... rubriekding
+=======
+>>>>>>> parent of 5963d42... Merge branch 'master' of https://github.com/Simonvn1/IProject-12
 <div class="uk-padding-small uk-nav uk-dropdown-nav">
   <h3>postzegels</h3>
   <ul>
     <li>europa</li>
     <li>amerika</li>
   </ul>
+<<<<<<< HEAD
 </div>
 </div>
 
@@ -21,6 +25,19 @@
     <li>europa</li>
     <li>amerika</li>
   </ul>
+=======
+</div>
+</div>
+
+<div>
+<div class="uk-padding-small uk-nav uk-dropdown-nav">
+  <h3>postzegels</h3>
+  <ul>
+    <li>europa</li>
+    <li>amerika</li>
+  </ul>
+</div>
+>>>>>>> parent of 5963d42... Merge branch 'master' of https://github.com/Simonvn1/IProject-12
 </div>
 </div>
 
