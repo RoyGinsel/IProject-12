4bef8e690945e55aa2b50334d549a46921053e62
RoyGinsel <36151106+RoyGinsel@users.noreply.github.com>
2018-05-07 13:15:16 +0200

    wef

--- Website/paginas/Includes/Rubriekenboom-header-dropdown.php
+++ Website/paginas/Includes/Rubriekenboom-header-dropdown.php
@@ -1,16 +1,30 @@
-<div class="uk-padding-small">
+
+<div>
+<div class="uk-padding-small uk-nav uk-dropdown-nav">
   <h3>postzegels</h3>
   <ul>
     <li>europa</li>
     <li>amerika</li>
   </ul>
 </div>
-<div class="uk-padding-small">
-  <h1>hallogaloafw</h1>
 </div>
-<div class="uk-padding-small">
-  <h1>hallogaloafw</h1>
+
+<div>
+<div class="uk-padding-small uk-nav uk-dropdown-nav">
+  <h3>postzegels</h3>
+  <ul>
+    <li>europa</li>
+    <li>amerika</li>
+  </ul>
 </div>
-<div class="uk-padding-small">
-  <h1>hallogaloafw</h1>
+</div>
+
+<div>
+<div class="uk-padding-small uk-nav uk-dropdown-nav">
+  <h3>postzegels</h3>
+  <ul>
+    <li>europa</li>
+    <li>amerika</li>
+  </ul>
+</div>
 </div>
--- Website/paginas/Includes/header.php
+++ Website/paginas/Includes/header.php
@@ -17,13 +17,26 @@
           <li><a href="product.php">Product</a></li>
         </ul>
       </div>
-      <div class="uk-button-group uk-inline uk-visible@m " uk-dropdown-grid>
+      <div class="uk-button-group uk-inline uk-visible@m ">
         <button class="uk-button uk-button-default" type="button" name="button"><span uk-icon="icon:  triangle-down">Dropdown</span></button>
-        <div class="uk-visible-small uk-child-width-1-2"uk-dropdown="mode: click; boundary: ! .uk-button-group; boundary-align: true;" uk-grid>
+        <div class="uk-width-large" uk-dropdown="mode: click; boundary: ! .uk-button-group; boundary-align: true;">
+          <div class="uk-dropdown-grid uk-child-width-1-2"uk-grid>
+            
+            <!-- search -->
+            <div class="uk-margin-remove">
+               <!-- php pagina met rubrieken -->
+                <form class="uk-search uk-search-default" action="">
+                    <a href="" uk-search-icon></a>
+                    <input class="uk-search-input" type="search" name="search" placeholder="Search...">
+                </form>
+            </div>
+
             <?php include "includes/Rubriekenboom-header-dropdown.php" ?>
-        </div>
+      </div>
       </div>
     </div>
+  </div>
+
     <h1 class="uk-align-right uk-margin-medium-top uk-margin-small-right"><a href="index.php"> Eenmaal Andermaal</a></h1>
   </div>
 
