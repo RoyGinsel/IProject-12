99eb439b0b83ec72d4e03d7f66d428246cf9f7d5
Simonvn1 <simon@vannoppen.com>
2018-05-07 15:19:30 +0200

    rubriekding

--- Website/paginas/Includes/Rubriekenboom-header-dropdown.php
+++ Website/paginas/Includes/Rubriekenboom-header-dropdown.php
@@ -1,30 +1,41 @@
 
-<div>
 <div class="uk-padding-small uk-nav uk-dropdown-nav">
   <h3>postzegels</h3>
-  <ul>
+  <ul class="rubriekenitem">
     <li>europa</li>
     <li>amerika</li>
   </ul>
-</div>
 </div>
 
-<div>
 <div class="uk-padding-small uk-nav uk-dropdown-nav">
   <h3>postzegels</h3>
-  <ul>
+  <ul class="rubriekenitem">
     <li>europa</li>
     <li>amerika</li>
   </ul>
-</div>
 </div>
 
-<div>
 <div class="uk-padding-small uk-nav uk-dropdown-nav">
   <h3>postzegels</h3>
-  <ul>
+  <ul class="rubriekenitem">
     <li>europa</li>
     <li>amerika</li>
   </ul>
 </div>
+
+<div class="uk-padding-small uk-nav uk-dropdown-nav">
+  <h3>postzegels</h3>
+  <ul class="rubriekenitem">
+    <li>europa</li>
+    <li>amerika</li>
+  </ul>
+</div>
+
+
+<div class="uk-padding-small uk-nav uk-dropdown-nav">
+  <h3>postzegels</h3>
+  <ul class="rubriekenitem">
+    <li>europa</li>
+    <li>amerika</li>
+  </ul>
 </div>
