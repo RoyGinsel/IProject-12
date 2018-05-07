9c63ec909a4dc50e0acde2f190539a26b3be875a
Simonvn1 <simon@vannoppen.com>
2018-05-07 13:13:31 +0200

    pagination

--- Website/paginas/Producten.php
+++ Website/paginas/Producten.php
@@ -1,6 +1,36 @@
 <?php
 session_start();
 $crumbs = array("Producten");
+if(isset($_GET["data"]))
+{
+    $data = $_GET["data"];
+} else {
+  $data = 1;
+}
+$pagination = "<ul class='uk-pagination'>";
+if($data != 1){
+  $prev = $data-1;
+  $pagination .= "<li><a href='producten.php?data=1'><<</span></a></li>
+                  <li><a href='producten.php?data=$prev'><</a></li>";
+}
+if($data == 2){
+  $pagination .= "<li><a href='producten.php?data=1'>1</a></li>";
+} elseif($data > 2){
+  $prev1 = $data - 1;
+  $prev2 = $data - 2;
+  $pagination .= "<li><a href='producten.php?data=$prev2'>$prev2</a></li>
+                  <li><a href='producten.php?data=$prev1'>$prev1</a></li>";
+}
+$next1 = $data+1;
+$next2 = $data+2;
+$pagination .= "<li class='uk-active'><span>$data</span></li>
+                <li><a href='producten.php?data=$next1'>$next1</a></li>
+                <li><a href='producten.php?data=$next2'>$next2</a></li>
+                <li><a href='producten.php?data=$next1'>></a></li>
+                </ul>";
+
+
+// zoek query met data invoeren om volgende producten te krijgen
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +49,11 @@ $crumbs = array("Producten");
   include "includes/header.php";
 ?>
 <main>
-  
+
+<div class="uk-flex uk-flex-center">
+<?php echo $pagination ?>
+</div>
+
 </main>
 <?php
   include "includes/footer.php";
