9b00778ca4bc1ad937fe78455d95b80327239fa2
Simonvn1 <simon@vannoppen.com>
2018-05-08 13:29:48 +0200

    rubriek sql

--- Database SQL/Select scripts.sql
+++ Database SQL/Select scripts.sql
@@ -19,3 +19,14 @@ where v.voorwerpNummer in (select top 2 v.voorwerpNummer
 							from tblBod
 							group by voorwerpNummer) b on v.voorwerpNummer=b.voorwerpNummer
 							order by bodBedrag-startPrijs desc)
+
+select h.rubriekNaam as hoofdRubriek, s.rubriekNaam as subRubriek
+from tblRubriek h 
+	inner join tblRubriek s on h.rubriekNummer=s.parentRubriek
+where h.parentrubriek= -1
+order by h.rubriekNaam asc
+
+
+
+select rubrieknaam from tblRubriek
+where parentRubriek = -1
