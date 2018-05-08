f97e71828986a982910c3809686543caebdd1d5c
RoyGinsel <36151106+RoyGinsel@users.noreply.github.com>
2018-05-08 11:22:34 +0200

    RUBRIEK DROPDOWN

--- Website/paginas/Includes/Rubriekenboom-header-dropdown.php
+++ Website/paginas/Includes/Rubriekenboom-header-dropdown.php
@@ -8,7 +8,7 @@
     </form>
 </div>
 
-<div class="uk-flex uk-flex-wrap uk-flex-space-around uk-width-1-1 uk-child-width-1-2 uk-dropdown-scrollable">
+<div class="uk-padding-remove uk-height-large uk-overflow-auto uk-flex  uk-flex-wrap uk-flex-space-around uk-width-1-1 uk-child-width-1-2">
   <?php
 foreach (rubrieken() as $waarde) {
   echo '
