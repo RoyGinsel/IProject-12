abbea2e1dee458c850204688467e5464ffa341e4
RoyGinsel <36151106+RoyGinsel@users.noreply.github.com>
2018-05-08 16:23:29 +0200

    Dropdown accordion

--- Website/paginas/Includes/header.php
+++ Website/paginas/Includes/header.php
@@ -19,8 +19,8 @@
       </div>
       <!-- Rubrieken dropdown medium & larger -->
       <span class="uk-visible@s">
-      <button type="button" class="rubrieken uk-button uk-button-small uk-padding-medium uk-margin-small-left uk-text-capitalize">Rubrieken</button>
-      <div class=" uk-width-1-2 uk-padding-remove-left uk-padding-remove-right uk-margin-remove-left uk-margin-remove-right uk-child-width-1-3@M" uk-dropdown="mode: click" uk-grid>
+      <button type="button" class="uk-button uk-button-small uk-padding-remove uk-margin-small-left">Rubrieken</button>
+      <div class=" uk-width-3-4 uk-padding-remove-left uk-padding-remove-right uk-margin-remove-left uk-margin-remove-right uk-child-width-1-3@M" uk-dropdown="mode: click" uk-grid>
           <?php include "Rubriekenboom-header-dropdown.php"?>
       </div>
     </div>
