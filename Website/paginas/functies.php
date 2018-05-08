87d110c3a172f4b9e6255fef11b781dffb0b6ab1
luukjansen17 <38424019+luukjansen17@users.noreply.github.com>
2018-05-08 11:39:49 +0200

    query

--- Website/paginas/functies.php
+++ Website/paginas/functies.php
@@ -16,8 +16,19 @@ function query($stringquery)
 
 function populaireitems()
 {
-return query("SELECT titel,beschrijving,startPrijs FROM tblVoorwerp");
+return query("select titel, beschrijving, b.bodBedrag
+from tblVoorwerp v
+inner join (select voorwerpNummer, max(bodBedrag) as bodBedrag
+			from tblBod
+			group by voorwerpNummer) b on v.voorwerpNummer=b.voorwerpNummer
+where v.voorwerpNummer in(select top 2 voorwerpNummer
+							from tblBod
+							group by voorwerpNummer
+							order by count(voorwerpnummer) desc)");
 }
+
+
+
 
 //Index.php -> Select statement voor uitgelichteitems
 function uitgelichteitems()
