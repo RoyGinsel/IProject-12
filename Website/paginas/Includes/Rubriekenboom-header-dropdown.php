bf53c9b268a48ef000f110250fb12afc471149d0
RoyGinsel <36151106+RoyGinsel@users.noreply.github.com>
2018-05-09 11:36:11 +0200

    producten rubrieken filter

--- Website/paginas/Includes/Rubriekenboom-header-dropdown.php
+++ Website/paginas/Includes/Rubriekenboom-header-dropdown.php
@@ -9,17 +9,30 @@
 </div>
 
 <div class="uk-padding-remove uk-height-large uk-overflow-auto uk-flex  uk-flex-wrap uk-flex-space-around uk-width-1-1 uk-child-width-1-2">
-  <?php
-  $rubriek = "";
-  foreach(rubrieken(-1) as $waarde){
-    $rubriek .=  "<ul uk-accordion> <li><div class='uk-padding-small uk-nav uk-dropdown-nav'> <a class='uk-accordion-title' >"
-    .$waarde['rubriekNaam']."</a> <div class='uk-accordion-content'><ul class='uk-list uk-list-striped'>";
-    foreach(rubrieken($waarde['rubriekNummer'])as $sub){
-      $rubriek .= "<li>".$sub['rubriekNaam']."</li>";
-    }
-    $rubriek .= "</ul></div></div></ul>";
+   <?php
+  // $rubriek = "";
+  // foreach(rubrieken(-1) as $waarde){
+  //   $rubriek .=  "<ul uk-accordion> <li><div class='uk-padding-small uk-nav uk-dropdown-nav'> <a class='uk-accordion-title' >"
+  //   .$waarde['rubriekNaam']."</a> <div class='uk-accordion-content'><ul class='uk-list uk-list-striped'>";
+  //   foreach(rubrieken($waarde['rubriekNummer'])as $sub){
+  //     $rubriek .= "<li>".$sub['rubriekNaam']."</li>";
+  //   }
+  //   $rubriek .= "</ul></div></div></ul>";
+  // }
+  // echo $rubriek;
+?>
+
+<?php
+$rubriek = "";
+foreach(rubrieken(-1) as $waarde){
+  $rubriek .=  "<ul uk-accordion> <li> <a class='uk-accordion-title' >"
+  .$waarde['rubriekNaam']."</a> <div class='uk-accordion-content'><ul class='uk-list uk-list-striped'>";
+  foreach(rubrieken($waarde['rubriekNummer'])as $sub){
+    $rubriek .= "<li>".$sub['rubriekNaam']."</li>";
   }
-  echo $rubriek;
+  $rubriek .= "</ul></li></ul>";
+}
+echo $rubriek;
 ?>
 
 </div>
