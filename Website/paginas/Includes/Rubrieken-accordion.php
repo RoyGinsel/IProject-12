873532903fc7124719a523aa5cd18a61f4607d00
RoyGinsel <36151106+RoyGinsel@users.noreply.github.com>
2018-05-15 13:22:04 +0200

    asdfjio

--- Website/paginas/Includes/Rubrieken-accordion.php
+++ Website/paginas/Includes/Rubrieken-accordion.php
@@ -1,10 +1,36 @@
 <?php
+//   $section = "";
+//   $parentrubriek= -1;
+//   while(sections($parentrubriek) != NULL){
+//   foreach(sections($parentrubriek) as $value) {
+//     $section .=  "<ul uk-accordion class='rubriekenlijst'> <li> <a class='uk-accordion-title'>"
+//     .$value['rubriekNaam']."</a> <div class='uk-accordion-content'><ul class='uk-list uk-list-striped'>";
+//        $parentrubriek = $value['rubriekNummer'];
+//        foreach(sections($value['rubriekNummer'])as $sub){
+//          $section .=  "<li><ul uk-accordion class='rubriekenlijst'> <li> <a class='uk-accordion-title'>"
+//          .$value['rubriekNaam']."</a> <div class='uk-accordion-content'><ul class='uk-list uk-list-striped'></li>";
+//   }
+//   // $section .= "<li> <a class='uk-link-reset' href='producten.php?rubriek=".$sub['rubriekNaam']."'>".$sub['rubriekNaam']."</a></li>";
+// $section .= "</ul></li></ul>";
+// }
+// echo $section;
+// }
+?>
+
+
+<?php
+$parentrubriek= -1;
   $section = "";
-  foreach(sections(-1) as $value){
+  foreach(sections($parentrubriek) as $value){
     $section .=  "<ul uk-accordion class='rubriekenlijst'> <li> <a class='uk-accordion-title'>"
     .$value['rubriekNaam']."</a> <div class='uk-accordion-content'><ul class='uk-list uk-list-striped'>";
-    foreach(sections($value['rubriekNummer'])as $sub){
-      $section .= "<li> <a class='uk-link-reset' href='producten.php?rubriek=".$sub['rubriekNaam']."'>".$sub['rubriekNaam']."</a></li>";
+    foreach(sections($value['rubriekNummer']) as $sub){
+      if((sections($parentrubriek) != NULL)){
+        $parentRubriek = $sub['RubriekNummer'];
+        $section .= "<li><div class='uk-accordion-content'><ul uk-accordion><li> </li>";
+      }
+      else{
+       $section .= "<li> <a class='uk-link-reset' href='producten.php?rubriek=".$sub['rubriekNaam']."'>".$sub['rubriekNaam']."</a></li>";
     }
   $section .= "</ul></li></ul>";
   }
