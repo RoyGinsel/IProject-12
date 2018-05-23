a7deab4e5834bde681445f51bee99d3aaccb05e7
Simonvn1 <simon@vannoppen.com>
2018-05-23 10:55:24 +0200

    PUUUUUUSH

---
+++ Beheersapplicatie/Rubrieken-Beheer.php
@@ -0,0 +1,30 @@
+<?php
+include "includes/functies.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script type="text/javascript" src="../Website/css/uikit-3.0.0-beta.42/dist/js/uikit.min.js"></script>
+    <script type="text/javascript" src="../Website/css/uikit-3.0.0-beta.42/dist/js/uikit-icons.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../Website/css/uikit-3.0.0-beta.42/dist/css/uikit.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Work+Sans:600" rel="stylesheet">
+    <link rel="stylesheet" href="../Website/css/style.css">
+</head>
+<body>
+    <?php
+        include "includes/header.php";
+    ?>
+<div class="uk-flex uk-flex-wrap uk-flex-space-around uk-width-1-1 uk-child-width-1-2" >
+    <div class="uk-padding">verwijderen</div>
+    <div class="uk-padding">hernoemen</div>
+    <div class="uk-padding">iets voor de toekomst</div>
+    <div class="uk-padding"  >iets anders voor de toekomsts</div>
+</div>    
+</body>
+</html>
+
