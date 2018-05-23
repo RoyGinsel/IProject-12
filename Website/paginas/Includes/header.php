a5090daf999c0c738c41e7199f226612d4b54595
RoyGinsel <36151106+RoyGinsel@users.noreply.github.com>
2018-05-23 13:51:03 +0200

    jquery

--- Website/paginas/Includes/header.php
+++ Website/paginas/Includes/header.php
@@ -1,3 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 <?php
   $breadcrumb = "<li><a href='index.php'>Home</a></li>";
   foreach($crumbs as $crumb) {
