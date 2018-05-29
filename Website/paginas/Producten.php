dbb6ebd709edd70640a7c20f3d5881c809cf6454
luukjansen17 <38424019+luukjansen17@users.noreply.github.com>
2018-05-29 16:27:17 +0200

    layout overal

--- Website/paginas/Producten.php
+++ Website/paginas/Producten.php
@@ -107,12 +107,12 @@
                     $lijst .= "
                     <tr>
                     <td><img class='uk-preserve-width uk-border-rounded' src=../media/Hamburgermenu.png width='80' alt=''>
-                    <h3 class='uk-text-top uk-margin-remove uk-text-bold uk-text-small'>".$waarde['titel']."</h3></td>
+                    <h3 class='uk-text-top uk-margin-small-left  uk-margin-remove-top uk uk-text-bold uk-text-small'>".$waarde['titel']."</h3></td>
                     <td class='uk-visible@s uk-text-break uk-text-nowrap uk-text-truncate'>
                     <h4 class='uk-text-small'>".$waarde['beschrijving']."</h4>
                     </td>
-                    <td class='uk-visible'>".$waarde['bodBedrag']."</td>
-                    <td><a class='uk-button uk-button-default' type='button' href='detailpagina.php?item=".$waarde['voorwerpNummer']."'>Ga naar bieding</a></td>
+                    <td class='uk-visible'>€ ".$waarde['bodBedrag']."</td>
+                    <td class='productenMobile'><a class='uk-button uk-text-small' type='button' href='detailpagina.php?item=".$waarde['voorwerpNummer']."'>Ga naar bieding</a></td>
                     </tr>";
                   }
                   echo $lijst;
