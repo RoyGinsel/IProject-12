babaa05f0d80bd1fc0d38ba79989c42be005b682
dexdieterman <38424097+dexdieterman@users.noreply.github.com>
2018-06-05 09:25:19 +0200

    Revert "mooiermaken layout producten enz"
    This reverts commit 38eb83ef68ac997d405db405c3002ada8bdc3670.

--- Website/paginas/Includes/Rubriekenboom-header-dropdown.php
+++ Website/paginas/Includes/Rubriekenboom-header-dropdown.php
@@ -16,14 +16,11 @@
 </div>
 
 <!-- Rubrieken accordion -->
-<div class="uk-padding-remove uk-height-large uk-overflow-auto uk-flex uk-flex-wrap uk-flex-space-around uk-width-1-1 uk-child-width-1-1 ">
+<div class="uk-padding-remove uk-height-large uk-overflow-auto uk-flex  uk-flex-wrap uk-flex-space-around uk-width-1-1 uk-child-width-1-2">
   <?php
   include "includes/Rubrieken-accordion.php";
 ?>
 </div>
-
-
-
 
 <!-- Javascript -->
 <script type="text/javascript">
